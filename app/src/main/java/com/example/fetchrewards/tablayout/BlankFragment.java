432cd8399d6a20d1aeffd20d2f425f4188afec67
YoshQ <xyoshqx@hotmail.com>
2023-03-22 15:20:19 -0500

    passing array from MainActivity to BlankFragment

--- app/src/main/java/com/example/fetchrewards/tablayout/BlankFragment.java
+++ app/src/main/java/com/example/fetchrewards/tablayout/BlankFragment.java
@@ -4,23 +4,34 @@ package com.example.fetchrewards.tablayout;
 import android.os.Bundle;
 //import android.support.design.widget.FloatingActionButton;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BlankFragment extends Fragment {
+//public class BlankFragment {
 
-
+    //MainActivity mainActivity = new MainActivity();
+    private MainActivity mainActivity;
     public BlankFragment() {
         // Required empty public constructor
+        mainActivity = new MainActivity();
+        List<Integer> list = mainActivity.getList();
+        //hurray !
+        Log.i("creation2","list size: " + list.size());
+        Log.i("creation2","list item: " + list.get(1));
     }
 
 
-    @Override
+    /*@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
@@ -47,7 +58,21 @@ public class BlankFragment extends Fragment {
                 dialog.show();
             }
         });*/
-        return v;
+        //new MainActivity.Sample() sample = MainActivity.Sample();
+        //MainActivity mainActivity = new MainActivity();
+        //MainActivity.Sample();
+        //ArrayList<String> listContent = new ArrayList<String>(mainActivity.list);
+        //ArrayList<String> listContent = mainActivity.getList();
+        //List<String> listContent = mainActivity.getList();
+        //List<Integer> listContent = mainActivity.getList();
+
+        //Log.i("creation2","list item: " + listContent.get(1));
+        //Log.i("creation2","list size: " + listContent.size());
+        /*return v;
     }
+
+    public BlankFragment() {
+
+    }*/
 
 }
--- app/src/main/java/com/example/fetchrewards/tablayout/MainActivity.java
+++ app/src/main/java/com/example/fetchrewards/tablayout/MainActivity.java
@@ -13,16 +13,44 @@ import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    //public List<String> list;
+    //public ArrayList<String> list = new ArrayList<>();
+    //private List<String> list;
+    private List<Integer> list;
     private ViewPager viewPager;
     private TabLayout tb;
     private TabItem t1,t2,t3;
+
+    public MainActivity() {
+        list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //list = new ArrayList<String>();
+        //list = new ArrayList<Integer>();
+
+        //list.add("Ammara Rasheed");
+        //list.add("Sara Khan");
+        //list.add("Maham Saleem");
+        //list.add("Ayesha Bibi");
+
+        //list.add(1);
+        //list.add(2);
+        //list.add(3);
+        //list.add(4);
         tb=findViewById(R.id.tab);
         viewPager=findViewById(R.id.viewPager);
         PageAdapter pageAdapter=new PageAdapter(getSupportFragmentManager(),tb.getTabCount());
@@ -54,5 +82,77 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        //this.Sample();
+        //com.example.fetchrewards.tablayout.Sample();
+        //list.add("Ammara Rasheed");
+        //list.add("Sara Khan");
+        //list.add("Maham Saleem");
+        //list.add("Ayesha Bibi");
+
+        //Log.i("creation2","list item: " + list.get(1));
+    }
+    /*
+     * This class will add elements to
+     * a shared list that is created with an Arraylist.
+     */
+
+    //class Write {
+        //private List<String> list;
+        // Receive a shared list in constructor
+        //Write(List<String> list) {
+            //this.list = list;
+            //list = list;
+        //}
+        // add items
+        //public void addItem(String item) {
+            //this.list.add(item);
+        //}
+    //}
+
+    //init
+    /*
+     * Read class will read elements form a shared list
+     */
+    /*class Read {
+
+        private List<String> list;
+
+        // Receive shared list in constructor
+        Read(List<String> list) {
+            this.list = list;
+        }
+
+        // Read the items
+        public void readItem() {
+
+            for (String item : list) {
+                //System.out.println(item.toString());
+                Log.i("creation2","item.toString(): " + item.toString());
+            }
+        }
+    }
+    // Test class.
+    public class Sample{
+        public void main(String[] args) {
+
+            // Use synchronized list to avoid exception
+            List<String> list = Collections
+                    .synchronizedList(new ArrayList<String>());
+
+            Write w = new Write(list);
+            Read r = new Read(list);
+
+            w.addItem("Apple");
+            w.addItem("Mange");
+
+            r.readItem();
+        }
+    }*/
+
+    //public ArrayList<String> getList() {
+    //public List<String> getList() {
+    public List<Integer> getList() {
+        return list;
+        //return Collections.unmodifiableList( list );
     }
 }
