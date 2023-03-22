17dadb1bbf7664422b7ae480c21e2c8a29f59b69
YoshQ <xyoshqx@hotmail.com>
2023-03-22 00:41:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/fetchrewards/tablayout/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.fetchrewards.tablayout;
+
+import android.os.Build;
+//import android.support.design.widget.TabItem;
+//import android.support.design.widget.TabLayout;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+//import android.support.v4.content.ContextCompat;
+import androidx.core.content.ContextCompat;
+//import android.support.v4.view.ViewPager;
+import androidx.viewpager.widget.ViewPager;
+//import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabLayout tb;
+    private TabItem t1,t2,t3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tb=findViewById(R.id.tab);
+        viewPager=findViewById(R.id.viewPager);
+        PageAdapter pageAdapter=new PageAdapter(getSupportFragmentManager(),tb.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+        tb.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if(tab.getPosition()==1){
+                    if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+                        getWindow().setStatusBarColor(ContextCompat.getColor(MainActivity.this,R.color.green1));
+                    }
+                }
+                if(tab.getPosition()==2){
+                    if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+                        getWindow().setStatusBarColor(ContextCompat.getColor(MainActivity.this,R.color.green2));
+                    }
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
